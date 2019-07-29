80a77f83db306cd95eb4fa981c3e6883d6246c34
Scrubi19 <scrubi.memo@yandex.ru>
2019-07-29 17:48:33 +0700

    Refreshing labs..

--- Labs/src/ru/eltex/laba5/Main.java
+++ Labs/src/ru/eltex/laba5/Main.java
@@ -1,7 +1,6 @@
 package ru.eltex.laba5;
 
 public class Main {
-
     public static void main(String[] args) {
 
     }
--- root-home/service/src/main/java/ru/bulatov/service/Main.java
+++
@@ -1,9 +0,0 @@
-package ru.bulatov.service;
-import ru.bulatov.utils.Man;
-
-public class Main {
-    public static void main(String[] args) {
-        Man man = new Man();
-    }
-
-}
