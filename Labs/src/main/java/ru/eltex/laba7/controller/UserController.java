d590b6cdda17e850dd19bceaa26769dfb9783b0d
Scrubi19 <scrubi.memo@yandex.ru>
2019-08-23 13:33:08 +0700

    laba6_final

--- Labs/src/main/java/ru/eltex/laba7/controller/UserController.java
+++
@@ -1,7 +0,0 @@
-package ru.eltex.laba7.controller;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class UserController {
-}
---
+++ root/src/main/java/ru/eltex/laba7/controller/UserController.java
@@ -0,0 +1,7 @@
+package ru.eltex.laba7.controller;
+
+//import org.springframework.stereotype.Controller;
+//
+//@Controller
+public class UserController {
+}
